75c8b8525e34affa0875d9034253d67e02af6c61
lukas <luki.minda@gmail.com>
2024-09-01 18:57:49 +0200

    Funkcia observer a práca z objektami

--- src/app/Components/AboutMe.js
+++ src/app/Components/AboutMe.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from 'react';
+
 import "./AboutMe.css";
 import AboutMeCard from './AboutMeCard';
 
@@ -11,56 +11,24 @@ import AboutMeCard from './AboutMeCard';
 
 
 const AboutMe = () => {
-    const [isInView, setIsInView] = useState(false);// mame nejaky state react premmennu.
-    const myRef = useRef(null);// use ref je funkcia v reacte a vytvorime refenciu niaky vzťah s html elementom
-    const myOpacity = isInView ? 1 : 0;
-    // logic
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                if (entries[0].isIntersecting) {
-                    setIsInView(true);
-                    observer.disconnect(); // Stop observing once the element is in view
-                }
-            },
-            {
-                threshold: 0.1, // Adjust this as needed, 0.1 means 10% of the element needs to be in view
-            }
-        );
-
-        if (myRef.current) {
-            observer.observe(myRef.current);
-        }
-
-        return () => {
-            if (myRef.current) {
-                observer.unobserve(myRef.current);
-            }
-        };
-    }, []);
-
-    // domáca uloha je  zvlášť po sebe aby sa zobrazili tie štyri divka komponenty
-    // a plus 
 
 
-
-
-
-
-
-
+    /*
+    
+   // domáca uloha je  zvlášť po sebe aby sa zobrazili tie štyri divka komponenty
+   // čiže ak prejdeme terat tak nam vypiše že in in view a zobrazia sa všetky divka naraz
+   // musime nejak nastaviť aby ak budeme na nej
+   // a plus 
+*/
     //html elements and styling
     return (
         <div id="description">
-            {isInView ? "is in view" : "is not in view."}
+
 
 
             <h3>Kto som?</h3>
-            <div className='text_about' ref={myRef} style={{
-                opacity: myOpacity,
-                transform: isInView ? 'translateX(0px)' : 'translateX(-100px)',
-                transition: 'transform 0.5s ease-out',
-            }} >
+            <div className='text_about'>
+
 
                 <AboutMeCard text="Som technicky zameraný samouk s medzinárodnými skúsenosťami." />
                 <AboutMeCard text="Hľadám príležitosť profesionálne rásť ako Front-end developer po rokoch individuálneho štúdia." />
