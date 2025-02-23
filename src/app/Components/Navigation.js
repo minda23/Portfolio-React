2edb29fc316d48984dcab213cb8f571bb70d890c
lukas <luki.minda@gmail.com>
2025-02-23 13:05:53 +0100

    changes

--- src/app/Components/Navigation.js
+++ src/app/Components/Navigation.js
@@ -24,6 +24,21 @@ const Navigation = (props) => {
         }
     };
 
+    const LanguageToggle = () => {
+        const { i18n } = useTranslation();
+
+        const toggleLanguage = () => {
+            const newLang = i18n.language === 'sk' ? 'en' : 'sk';
+            i18n.changeLanguage(newLang);
+        };
+
+        return (
+            <button onClick={toggleLanguage}>
+                {i18n.language === 'sk' ? 'Switch to English' : 'Prepnúť na slovenčinu'}
+            </button>
+        );
+    };
+
     return (
         <div>
             <div>
@@ -38,8 +53,8 @@ const Navigation = (props) => {
                     </ul>
                 </nav>
 
-                <LanguageToggle />
 
+                <LanguageToggle />
 
                 {isMobile && (
                     <div
