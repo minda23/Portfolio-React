a0c76099ef4395428b03b8e079bbcd5eaec9b8fd
lukas <luki.minda@gmail.com>
2025-02-24 18:16:58 +0100

    change

--- src/app/Components/Navigation.js
+++ src/app/Components/Navigation.js
@@ -24,8 +24,38 @@ const Navigation = (props) => {
 
     return (
         <div>
-            <div>
-                <nav id="navigation" className={isMenuOpen ? "menu open" : "menu"}>
+            {isMobile && (
+                <div
+                    id="burger_menu"
+                    onClick={toggleMenu}
+                    style={{ display: isMenuOpen ? "none" : "block" }}
+                >
+                    <img
+                        title="obrazok"
+                        src="/images/hamburger.png"
+                        alt="logo"
+                        width="50"
+                    />
+                </div>
+            )}
+
+            {isMobile && (
+                <div
+                    id="cross"
+                    onClick={toggleMenu}
+                    style={{ display: isMenuOpen ? "block" : "none" }}
+                >
+                    <img
+                        title="obrazok"
+                        src="/images/cross.svg"
+                        alt="logo"
+                        width="50"
+                    />
+                </div>
+            )}
+
+            {isMenuOpen && (
+                <nav id="navigation" className="menu open">
                     <ul>
                         <NavigationCard name="O mne" clickref={aboutMe} />
                         <NavigationCard name="Schopnosti" clickref={skills} />
@@ -34,37 +64,7 @@ const Navigation = (props) => {
                         <NavigationCard name="Kontakt" clickref={footer} />
                     </ul>
                 </nav>
-
-                {isMobile && (
-                    <div
-                        id="burger_menu"
-                        onClick={toggleMenu}
-                        style={{ display: isMenuOpen ? "none" : "block" }}
-                    >
-                        <img
-                            title="obrazok"
-                            src="/images/hamburger.png"
-                            alt="logo"
-                            width="50"
-                        />
-                    </div>
-                )}
-
-                {isMobile && (
-                    <div
-                        id="cross"
-                        onClick={toggleMenu}
-                        style={{ display: isMenuOpen ? "block" : "none" }}
-                    >
-                        <img
-                            title="obrazok"
-                            src="/images/cross.svg"
-                            alt="logo"
-                            width="50"
-                        />
-                    </div>
-                )}
-            </div>
+            )}
         </div>
     );
 };
